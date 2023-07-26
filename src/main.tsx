ee4fbca1be072539c8c000792fc27ed94dcf3508
李维/Information Service Team/ST <li_wei@nhn-st.com>
2023-07-26 09:59:24 +0800

    chroe: init project

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div>React App</div>
+  </React.StrictMode>,
+);
